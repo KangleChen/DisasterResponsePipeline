6d9fc74e0097de5d0011ebe368ca076556ffb7e3
Kangle Chen <kangle.chen1103@gmail.com>
2021-02-21 19:57:56 +0100

    complete ML Pipeline Preparation-Improv1

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,16 +1,82 @@
 import sys
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
+
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english')) 
+from nltk.stem import WordNetLemmatizer
+
+import re 
+from nltk import word_tokenize 
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report, f1_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 
 def load_data(database_filepath):
-    pass
+    """
+    Load data from database
+    Input: 
+    database_filepath (string): file path of the database 
+
+    Output:
+    X (matrix): X for training model 
+    y (matrix): y for taining model 
+    """
+
+    path_str = 'sqlite:///' + database_filepath
+    table_name = engine.table_names()
+    engine = create_engine(path_str)
+    df = pd.read_sql_table(table_name, con = engine)
+    X = df.iloc[:,1].values
+    y = df.iloc[:,5:-1].values
+    return X,y
 
 
 def tokenize(text):
-    pass
+    """
+    tokenize input text
+    Input: 
+    text (str): text to be tokenized 
+    Output:
+    clean_tokens(): results 
+    """
+    # sentence tokenize 
+    sentences = sent_tokenize(text)
+    
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for sent_ in sentences:
+        # normalization 
+        text = re.sub(r"[^a-zA-Z0-9]", " ", sent_.lower())
+        # tokenize 
+        words = word_tokenize(text)
+        # remove stop words 
+        words = [word for word in words if not word in stop_words]
+        # lemmatization
+        for word in words:
+            clean_tok = lemmatizer.lemmatize(word).lower().strip()
+            clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    
+
+    return cv_model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
